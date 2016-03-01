249c5a32951f71d721d47dd55f8dbff851ec25d4
iovigi <il.nedelchev@gmail.com>
2016-02-29 23:33:50 -0800

    CommentViewModel add

---
+++ PsychologicalGuide.Web.Models/Articles/CommentViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace PsychologicalGuide.Web.Models.Articles
+{
+    using System;
+    using AutoMapper;
+    using Data.Models.Information.Articles;
+    using Infrastructure.Mapping;
+
+    public class CommentViewModel : IMapFrom<ArticleComment>, IHaveCustomMappings
+    {
+        public string UserEmail { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<ArticleComment, CommentViewModel>()
+                .ForMember(p => p.UserEmail, opt => opt.MapFrom(p => p.User.Email))
+                .ForMember(p=> p.Date, opt=> opt.MapFrom(p=> p.CreatedOn));
+        }
+    }
+}
