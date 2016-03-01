9861ab05f54c13e750565ac966bf6c9e09b7644a
iovigi <il.nedelchev@gmail.com>
2016-02-29 19:10:54 -0800

    fix!

---
+++ PsychologicalGuide.Data.Services/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿namespace PsychologicalGuide.Data.Services
+{
+    using Models.Information.Articles;
+    using System.Linq;
+
+    public interface ICommentService
+    {
+        IQueryable<ArticleComment> AllForArticle(int articleId);
+
+        IQueryable<ArticleComment> All();
+
+        void Add(int articleId, string content, string userId);
+
+        void Delete(int id);
+
+        ArticleComment GetById(int id);
+    }
+}
